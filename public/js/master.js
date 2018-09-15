335007ebcd23caa2e2f5a4d609242ef6d30d76b5
Brian K. Kiragu <brian@amprest.co.ke>
2018-09-16 02:50:33 +0300

    Secondary bootstrapping

---
+++ public/js/master.js
@@ -0,0 +1,22 @@
+webpackJsonp([2],{
+
+/***/ 44:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(45);
+
+
+/***/ }),
+
+/***/ 45:
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+    "use strict";
+
+    // Code.
+});
+
+/***/ })
+
+},[44]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/icon/icon.png') }}">
-        <title>{{config('app.name')}}</title>
-        <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div id="app">
-            
-        </div>
-        <script src="{{mix('js/app.js')}}" ></script>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/icon/icon.png') }}">
+
+  <title>{{ config('app.name') }}</title>
+
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="app">
+    @yield('content')
+  </div>
+
+  <script src="{{ mix('js/manifest.js') }}" defer></script>
+  <script src="{{ mix('js/vendor.js') }}" defer></script>
+  <script src="{{ mix('js/app.js') }}" defer></script>
+  <script src="{{ mix('js/master.js') }}" defer></script>
+</body>
 </html>
