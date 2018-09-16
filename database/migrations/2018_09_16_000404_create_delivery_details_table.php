9f6dff9fc927bed3c8ea85b85295682ace1194cd
geekaburu <geekaburu@gmail.com>
2018-09-16 20:58:20 +0300

    Updates to delivery details migrations

--- database/migrations/2018_09_16_000404_create_delivery_details_table.php
+++ database/migrations/2018_09_16_000404_create_delivery_details_table.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateDeliveryDetailsTable extends Migration
 {
+
+    public function __construct() {
+        $this->drivers = ['mysql', 'pgsql', 'sqlsrv'];
+        $this->tables = [
+            ['delivery_details_1', 'delivery_details_2'],
+            ['delivery_details_3', 'delivery_details_4'],
+        ];
+    }
+
     /**
      * Run the migrations.
      *
@@ -13,44 +22,21 @@ class CreateDeliveryDetailsTable extends Migration
      */
     public function up()
     {
-        // Site 1.
-        Schema::connection('mysql')->create('delivery_details_1', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->unsignedInteger('delivery_id');
-            $table->float('weight');
-            $table->timestamps();
-        });
-        Schema::connection('mysql')->create('delivery_details_2', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->longText('description');
-            $table->timestamps();
-        });
-
-        // Site 2.
-        Schema::connection('pgsql')->create('delivery_details_1', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->unsignedInteger('delivery_id');
-            $table->float('weight');
-            $table->timestamps();
-        });
-        Schema::connection('pgsql')->create('delivery_details_2', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->longText('description');
-            $table->timestamps();
-        });
-
-        // Site 3.
-        Schema::connection('sqlsrv')->create('delivery_details_1', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->unsignedInteger('delivery_id');
-            $table->float('weight');
-            $table->timestamps();
-        });
-        Schema::connection('sqlsrv')->create('delivery_details_2', function     (Blueprint $table) {
-            $table->unsignedInteger('id');
-            $table->longText('description');
-            $table->timestamps();
-        });
+        foreach ($this->drivers as $driver) {
+            for ($i=0; $i <2 ; $i++) { 
+                Schema::connection($driver)->create($this->tables[$i][0], function (Blueprint $table) {
+                    $table->unsignedInteger('id');
+                    $table->unsignedInteger('delivery_id');
+                    $table->float('weight');
+                    $table->timestamps();
+                });
+                Schema::connection($driver)->create($this->tables[$i][1], function (Blueprint $table) {
+                    $table->unsignedInteger('id');
+                    $table->longText('description');
+                    $table->timestamps();
+                });
+            }
+        }
     }
 
     /**
@@ -60,11 +46,12 @@ class CreateDeliveryDetailsTable extends Migration
      */
     public function down()
     {
-        Schema::connection('mysql')->dropIfExists('delivery_details_1');
-        Schema::connection('mysql')->dropIfExists('delivery_details_2');
-        Schema::connection('pgsql')->dropIfExists('delivery_details_1');
-        Schema::connection('pgsql')->dropIfExists('delivery_details_2');
-        Schema::connection('sqlsrv')->dropIfExists('delivery_details_1');
-        Schema::connection('sqlsrv')->dropIfExists('delivery_details_2');
+        // Drop databases from all drivers
+        foreach ($this->drivers as $driver) {
+            Schema::connection($driver)->dropIfExists('delivery_details_1');
+            Schema::connection($driver)->dropIfExists('delivery_details_2');
+            Schema::connection($driver)->dropIfExists('delivery_details_3');
+            Schema::connection($driver)->dropIfExists('delivery_details_4');            
+        }
     }
 }
