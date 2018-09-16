b10b0a31b253be484430b42fcb97f9d8aeb4900f
Brian K. Kiragu <brian@amprest.co.ke>
2018-09-16 13:02:53 +0300

    Minor changes

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-laravel navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-laravel">
   <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <img src="{{ asset('img/icon/menu.svg') }}" alt="Menu Icon" class="img-fluid navbar-toggler-icon">
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
