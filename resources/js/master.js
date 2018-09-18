a3e8d97ce0457729d9b549d4c7e8dbe875e02dec
geekaburu <geekaburu@gmail.com>
2018-09-18 16:13:26 +0300

    Datatables options

--- resources/js/master.js
+++ resources/js/master.js
@@ -4,10 +4,16 @@ $(document).ready(function () {
     // Datatables initialization
     let table = $('#deliveries-table').DataTable({
     	"scrollX": "true",
+        "lengthMenu": [[20, 40, 60, -1], [20, 40, 60, 'All']],
+        "pageLength": 20,
     	"oLanguage": { "sEmptyTable": "No messages are available." },
         "columnDefs": [{ 
             type: 'date-eu', 
             targets: 0 
+        },
+        {
+            "targets": [ 8, 9, 10, 11 ],
+            "visible": false
         }],
         "order": [[ 0, "desc" ]]
     });
@@ -24,14 +30,33 @@ $(document).ready(function () {
         }
         else {
             // Open this row
-            row.child( format() ).show();
+            row.child( format(row.data()) ).show();
             tr.addClass('shown');
         }
     } );
 
     /* Formatting function for row details - modify as you need */
-    function format ( ) {
-        return '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui animi neque suscipit provident accusamus ipsum cupiditate deleniti, illum. Vitae praesentium illo est tenetur accusantium maiores incidunt excepturi nisi quis quam.</p>';
+    function format (data) {
+        let columns = data.slice(Math.max(data.length - 4, 1))
+        return `
+            <table class="table w-100 my-5">
+                <tr>
+                    <td>Sender Phone Number</td>
+                    <td>${ columns[0] }</td>
+                </tr>
+                <tr>
+                    <td>Recipient Phone Number</td>
+                    <td>${ columns[1] }</td>
+                </tr>
+                <tr>
+                    <td>Agent Name</td>
+                    <td>${ columns[2] }</td>
+                </tr>
+                <tr>
+                    <td>Details of the Goods</td>
+                    <td>${ columns[3] }</td>
+                </tr>
+            </table>`
     }
     
 });
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container">
+  <div class="container-fluid">
     <div class="row justify-content-center">
       <div class="col-md-12">
         <div class="card">
@@ -14,20 +14,22 @@
             @endif
 
             {{-- Details of each delivery --}}
-            <table id="deliveries-table" class="table table-striped">
+            <table id="deliveries-table" class="table table-striped w-100 table-sm">
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">#</th>
                   <th scope="col">Order No</th>
                   <th scope="col">Sender Name</th>
-                  <th scope="col">Sender Number</th>
                   <th scope="col">Sender Location</th>
                   <th scope="col">Recipient Name</th>
-                  <th scope="col">Recipient Number</th>
                   <th scope="col">Recipient Location</th>
-                  <th scope="col">Agent Name</th>
                   <th scope="col">Item Weight</th>
                   <th scope="col">Delivery Status</th>
+                  
+                  <th scope="col">Sender Number</th>
+                  <th scope="col">Recipient Number</th>
+                  <th scope="col">Agent Name</th>
+                  <th scope="col">Description</th>
                 </tr>
               </thead>
               <tbody>
@@ -36,28 +38,34 @@
                     <td class="details-control"></td>
                     <td>{{$item->delivery_no}}</td>
                     <td>{{$item->sender_name}}</td>
-                    <td>{{$item->sender_number}}</td>
                     <td>{{$item->sender_location}}</td>
                     <td>{{$item->recipient_name}}</td>
-                    <td>{{$item->recipient_number}}</td>
                     <td>{{$item->recipient_location}}</td>
+                    <td>{{$item->weight}} kg</td>
+                    <td>{{$item->delivery_status}}</td>  
+                                                          
+                    <td>{{$item->sender_number}}</td>
+                    <td>{{$item->recipient_number}}</td>
                     <td>{{$item->agent_name}}</td>
-                    <td>{{$item->weight}}</td>
-                    <td>{{$item->delivery_status}}</td>
+                    <td>{{$item->description}}</td>
                   </tr>
                 @endforeach
               </tbody>
               <tfoot> 
                 <tr>
                   <th scope="col">#</th>
+                  <th scope="col">Order No</th>
                   <th scope="col">Sender Name</th>
-                  <th scope="col">Sender Number</th>
                   <th scope="col">Sender Location</th>
                   <th scope="col">Recipient Name</th>
-                  <th scope="col">Recipient Number</th>
                   <th scope="col">Recipient Location</th>
-                  <th scope="col">Agent Name</th>
                   <th scope="col">Item Weight</th>
+                  <th scope="col">Delivery Status</th>
+                  
+                  <th scope="col">Sender Number</th>
+                  <th scope="col">Recipient Number</th>
+                  <th scope="col">Agent Name</th>
+                  <th scope="col">Description</th>
                 </tr>
               </tfoot>
           </div>
